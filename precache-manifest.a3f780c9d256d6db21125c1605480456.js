ffeb2cf5713cac3c6380751d120b793a8593bc3d
Nicolás Vásquez Murcia <n.vasquez10@uniandes.edu.co>
2019-01-30 16:53:02 -0500

    Updates

---
+++ precache-manifest.a3f780c9d256d6db21125c1605480456.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mywebsite/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "acc54b3174e7cd2ff196",
+    "url": "/mywebsite/static/js/runtime~main.acc54b31.js"
+  },
+  {
+    "revision": "f6c628345afc6af485e7",
+    "url": "/mywebsite/static/js/main.f6c62834.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/mywebsite/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "f6c628345afc6af485e7",
+    "url": "/mywebsite/static/css/main.2904ffcf.chunk.css"
+  },
+  {
+    "revision": "0b7e358ba8a7d8a70851f9e1ec42c2f5",
+    "url": "/mywebsite/index.html"
+  }
+];
